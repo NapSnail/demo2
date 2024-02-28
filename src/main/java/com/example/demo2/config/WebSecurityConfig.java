fbea7123afbf294250f0f0497ac45b61fdf7a2ed
YukaUeno <156647522+NapSnail@users.noreply.github.com>
2024-02-28 09:15:27 +0900

    認可機能の実装完了

--- src/main/java/com/example/demo2/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo2/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo2.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import com.example.demo2.data.service.AccountUserDetailsService;
 
 @Configuration // 設定クラス
 @EnableWebSecurity // Spring Securityを使う
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -30,7 +32,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests().antMatchers("/loginForm").permitAll().anyRequest().authenticated();
+		http.exceptionHandling()
+		.accessDeniedPage("/accessDeniedPage")
+		.and()
+		.authorizeRequests().antMatchers("/loginForm").permitAll().anyRequest().authenticated();
 		
 		
 		http.formLogin()
